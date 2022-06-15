4aa1734150d0f5b9014cdbd4181be8744da6d159
andrei12333 <abalanandreibalan8@yahoo.com>
2022-06-15 22:13:28 +0300

    Initial Update

--- DeveloperTools.py
+++ DeveloperTools.py
@@ -1,13 +1,14 @@
 import time
 import random
 from winotify import Notification, audio
+from tkinter import messagebox
 
-class MyModule():
+class DeveloperTools():
 
     def hello_world():
         print("Hello, World!")
 
-    def advancedprint(string, amount):
+    def advancedprint(string="Hello, World!", amount=1):
         count = 0
         while (count < amount):
             count = count + 1
@@ -21,5 +22,37 @@ class MyModule():
         toast.set_audio(sound, loop=AudioLoop)
         toast.show()
 
+    def wait(seconds=1, message="null"):
+        time.sleep(seconds)
+        if message != "null":
+            print(message)
+
+    def alert(title="Alert", description="Hello, World!", appname="Python"):
+        toast = Notification(app_id=appname, title=title, msg=description, duration="short")
+        toast.set_audio(audio.LoopingAlarm, loop=True)
+        toast.show()
+
+    def spam(message="Hello, World!"):
+        count = 0
+        while (count < 50):
+            count = count + 1
+            print(message)
+
+    def msgbox(information="Hello, World!", type="info", title="Hello, World!"):
+        if type == "info":
+            messagebox.showinfo(title, information)
+        if type == "warn":
+            messagebox.showwarning(title, information)
+        if type == "error":
+            messagebox.showerror(title, information)
+        if type == "askquestion":
+            messagebox.askquestion(title, information)
+        if type == "askokcancel":
+            messagebox.askokcancel(title, information)
+        if type == "askyesno":
+            messagebox.askyesno(title, information)
+        if type == "askretrycancel":
+            messagebox.askretrycancel(title, information)
+
     def __init__(self):
-        super(MyModule, self).__init__()
+        super(DeveloperTools, self).__init__()
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DeveloperTools
 A collection of functions for python developers.
+
+**Table of contents**
+
+* [DeveloperTools](https://github.com/ANDREI12333/DeveloperTools#developertools)
+    * [Installation](https://github.com/ANDREI12333/DeveloperTools/#installation)
+    * [LICENSE](https://github.com/ANDREI12333/DeveloperTools/#license)
+
+# Installation
+Run the following command in a terminal or a command prompt.
+```
+pip install DeveloperTools
+```
+# LICENSE
+**This project is being published using the MIT License, READ [LICENSE.md](https://github.com/ANDREI12333/DeveloperTools/blob/main/LICENSE.md) for more info.**
